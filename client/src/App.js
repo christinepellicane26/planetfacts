86a0281f8c1b7d63fc654c8b20d1c12865830383
christinepellicane26 <christinepellicane@gmail.com>
2019-08-07 15:30:29 -0400

    fixed some syntax issues

--- client/src/App.js
+++ client/src/App.js
@@ -48,26 +48,27 @@ class App extends Component {
 
       //for resizing purposes:
 
-{/* <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/19_mars.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png');"></div>
-<div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/stellar_items/list_view_images/90_neptune_480x320.jpg');"></div>    */}
-    
-    )
+      
+      )
+    }
   }
-}
-
-
-export default App;
-
+  
+  
+  export default App;
+  
   // const api_call = await fetch(`https://api....`)
   // const data = await this.api_call.json();
-
-//images: https://epic.gsfc.nasa.gov/about/api
-//planet data in French/metric: https://api.le-systeme-solaire.net/en/
-//https://api.le-systeme-solaire.net/rest/
-
+  
+  //images: https://epic.gsfc.nasa.gov/about/api
+  //planet data in French/metric: https://api.le-systeme-solaire.net/en/
+  //https://api.le-systeme-solaire.net/rest/
+  
+  
+  /* <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/19_mars.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png');"></div>
+  <div class="img" style="background-image:url('https://solarsystem.nasa.gov/system/stellar_items/list_view_images/90_neptune_480x320.jpg');"></div>    */
