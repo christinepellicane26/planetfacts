f496b56766dae6ec35e7f2da5874b764cfa9a145
christinepellicane26 <christinepellicane@gmail.com>
2019-08-07 17:05:15 -0400

    Added express madness

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const path = require('path')
 // app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', '../client/public')))
+app.use(express.static(path.join(__dirname, '..', '../client/public')));
+app.get('*', (req,res) => {
+  res.sendfile(path.join(__dirname+'/client/public/index.html'))
+})
 // app.use("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, '..', '../client/public/index.html'))
 // })
@@ -62,7 +65,10 @@ app.get('/api/planets/:name', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req,res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'))
+  })
   
 }
 
