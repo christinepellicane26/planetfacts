b6a5843f0c03a6229f2e154b79289498f3c397b4
christinepellicane26 <christinepellicane@gmail.com>
2019-08-07 16:55:32 -0400

    Fixed the client html route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  res.sendFile(path.join(__dirname, '..', '../client/public/index.html'))
 })
 
 
