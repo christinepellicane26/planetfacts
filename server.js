afa60f24202ecba78629e45f43bf2130d8bbe683
christinepellicane26 <christinepellicane@gmail.com>
2019-08-07 17:26:33 -0400

    Added a path

--- server.js
+++ server.js
@@ -64,13 +64,13 @@ app.get('/api/planets/:name', (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('/', (req,res) => {
-    res.sendfile(path.join(__dirname = '../client/build/index.html'))
-  })
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+//   app.get('/', (req,res) => {
+//     res.sendfile(path.join(__dirname = '../client/build/index.html'))
+//   })
   
-}
+// }
 
   // for (let el in solarSystem){
   //   if (el.name == planetString){
